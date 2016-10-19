118f850fbd16e56f8dd9bbdddaa6ad46f16a611a
devops-learner <em_sg_se@outlook.com>
2016-10-19 17:47:25 +0500

    Shop_Demo_applicatoin
    For Demo of
    -IdentityManager (Brac Allen)
    -UOW
    -Generic Repository

---
+++ Shop/Shop/Startup.cs
@@ -0,0 +1,70 @@
+﻿using IdentityManager.Configuration;
+using IdentityManager.AspNetIdentity;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Shop.Models;
+using IdentityManager;
+
+[assembly: OwinStartupAttribute(typeof(Shop.Startup))]
+namespace Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.Map("idm", idm =>
+            {
+                var factory = new IdentityManagerServiceFactory();
+                factory.IdentityManagerService = new Registration<IIdentityManagerService, ApplicationIdentityManagerService>();
+                factory.Register(new Registration<ApplicationUserManager>());
+                factory.Register(new Registration<ApplicationUserStore>());
+                factory.Register(new Registration<ApplicationRoleManager>());
+                factory.Register(new Registration<ApplicationRoleStore>());
+                factory.Register(new Registration<ApplicationDbContext>());
+
+
+                idm.UseIdentityManager(new IdentityManagerOptions
+                {
+                    Factory = factory
+                });
+
+            });
+
+        }
+    }
+
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(ApplicationRoleStore roleStore):base(roleStore)
+        {
+
+        }
+
+    }
+    public class ApplicationUserStore:UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(ApplicationDbContext ctx):base(ctx)
+        {
+
+        }
+    }
+
+    public class ApplicationRoleStore:RoleStore<IdentityRole>
+    {
+        public ApplicationRoleStore(ApplicationDbContext ctx) :base(ctx)
+        {
+
+        }
+    }
+
+    public class ApplicationIdentityManagerService:AspNetIdentityManagerService<ApplicationUser,string,IdentityRole,string>
+    {
+        public ApplicationIdentityManagerService(ApplicationUserManager userMgr, ApplicationRoleManager roleMgr) :base(userMgr,roleMgr)
+        {
+                
+        }
+    }
+}
